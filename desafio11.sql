9ed8a0545aa30a01cad4801589dfbb19c7a13dc1
Lauro Pereira <lauropera01@gmail.com>
2022-09-26 15:36:01 -0300

    refatorando desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,12 +1,13 @@
-SELECT
-song_name AS nome_musica,
-CASE
-	WHEN song_name LIKE '%Bard%' THEN REPLACE(song_name, 'Bard', 'QA')
-	WHEN song_name LIKE '%Amar%' THEN REPLACE(song_name, 'Amar', 'Code Review')
-	WHEN song_name LIKE '%Pais%' THEN REPLACE(song_name, 'Pais', 'Pull Requests')
-	WHEN song_name LIKE '%SOUL%' THEN REPLACE(song_name, 'SOUL', 'CODE')
-	WHEN song_name LIKE '%SUPERSTAR%' THEN REPLACE(song_name, 'SUPERSTAR', 'SUPERDEV')
-END AS novo_nome
-FROM SpotifyClone.song
-HAVING nome_musica <> novo_nome
+SELECT * FROM (
+	SELECT
+		song_name AS nome_musica,
+		CASE
+			WHEN song_name LIKE '%Bard%' THEN REPLACE(song_name, 'Bard', 'QA')
+			WHEN song_name LIKE '%Amar%' THEN REPLACE(song_name, 'Amar', 'Code Review')
+			WHEN song_name LIKE '%Pais%' THEN REPLACE(song_name, 'Pais', 'Pull Requests')
+			WHEN song_name LIKE '%SOUL%' THEN REPLACE(song_name, 'SOUL', 'CODE')
+			WHEN song_name LIKE '%SUPERSTAR%' THEN REPLACE(song_name, 'SUPERSTAR', 'SUPERDEV')
+		END AS novo_nome
+	FROM SpotifyClone.song) AS new_songs
+WHERE new_songs.novo_nome IS NOT NULL
 ORDER BY nome_musica DESC;
